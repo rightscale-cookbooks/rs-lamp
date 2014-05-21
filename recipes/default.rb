28158d948cb3cf69bdacde80a3332176ee8a7a85
Lopaka Delp <lopaka@rightscale.com>
2014-05-20 20:24:14 -0700

    Added a few more attributes to override for LAMP.

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,6 +24,14 @@ end
 Chef::Log.info "Overriding rs-mysql/server_usage to 'shared'"
 node.override['rs-mysql']['server_usage'] = 'shared'
 
+Chef::Log.info "Overriding rs-mysql/bind_network_interface to 'private'"
+node.override['rs-mysql']['bind_network_interface'] = 'private'
+
+# Volumes are not used so volume snapshots/backups are not done.
+# However, this attribute is still a requirement for rs-mysql::default recipe.
+Chef::Log.info "Overriding rs-mysql/backup/lineage to 'lamp-lineage'"
+node.override['rs-mysql']['backup']['lineage'] = 'lamp-lineage'
+
 Chef::Log.info "Overriding mysql/bind_address to 'localhost'"
 node.override['mysql']['bind_address'] = 'localhost'
 
@@ -32,3 +40,8 @@ node.override['rs-application_php']['database']['host'] = 'localhost'
 
 Chef::Log.info "Overriding rs-application_php/listen_php to 80"
 node.override['rs-application_php']['listen_port'] = 80
+
+# LAMP does not have a database replication feature.
+# However, this attribute is required by mysql::server which is called from rs-mysql::default.
+Chef::Log.info "Overriding rs-mysql/server_repl_password to 'not_applicable'"
+node.override['rs-mysql']['server_repl_password'] = 'not_applicable'
